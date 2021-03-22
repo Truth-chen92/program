337c4114e3ed0f5d772e24ed8f2de5d94ab0923e
Truth <527768695@qq.com>
2021-03-22 17:13:10 +0800

    小星星修改

--- src/main/java/cn/tedu/travel/controller/ShowController.java
+++ src/main/java/cn/tedu/travel/controller/ShowController.java
@@ -1,8 +1,18 @@
 package cn.tedu.travel.controller;
 
+import cn.tedu.travel.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.context.Context;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.security.Principal;
 
 @Controller
 @Slf4j
@@ -23,5 +33,11 @@ public class ShowController {
     public String showAbout(){return "about";}
     @GetMapping("/showcontactus")
     public String showContactUs(){return "contactUs";}
-
+    @GetMapping("/showhome")
+    public String showHomePage(HttpServletRequest request,HttpServletResponse response
+    ){
+        User user=(User)request.getSession().getAttribute("user");
+        System.out.println(user);
+        return "homePage";
+    }
 }
