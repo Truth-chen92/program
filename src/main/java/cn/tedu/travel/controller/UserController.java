fa2376c0ecc55c4bf6b54d8761af7d34af3a1617
uid <1487956913@qq.com>
2021-03-27 11:59:31 +0800

    cxl

--- src/main/java/cn/tedu/travel/controller/UserController.java
+++ src/main/java/cn/tedu/travel/controller/UserController.java
@@ -1,10 +1,34 @@
 package cn.tedu.travel.controller;
 
+import cn.tedu.travel.mapper.UserMapper;
+import cn.tedu.travel.model.User;
+import cn.tedu.travel.service.IUserService;
+import cn.tedu.travel.vo.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
 public class UserController {
 
+    @Resource
+    IUserService userService;
+
+    @GetMapping("/users")
+    public R<List<User>> showAllUsers(){
+        List<User> users = userService.list();
+        return R.ok(users).setCode(0);
+    }
+
+    @GetMapping("/deleteUser")
+    public R deleteUser(Integer id){
+        userService.removeById(id);
+        return R.ok("ok");
+    }
 }
